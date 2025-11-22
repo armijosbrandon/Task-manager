28924317c2404c4ef6e82cd422e382d1ce2f28f8
Brandon Armijos <armijosbarvecho@gmail.com>
2025-11-22 13:57:00 -0500

    incorporado Libreria de iconos

--- src/main/java/io/github/ArmijosBrandon/TaskManager/View.java
+++ src/main/java/io/github/ArmijosBrandon/TaskManager/View.java
@@ -4,7 +4,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-
+import org.kordamp.ikonli.javafx.FontIcon;
 public class View {
 	private Stage stage;
 	public View(Stage stage) {
@@ -13,8 +13,17 @@ public class View {
 	}
 
 	private void initializeUI() {
+//PANTALLA PRINCIPAL
+		//contenedor de titulo
+		HBox contTitulo = new HBox(new Label("Gestor de Tareas"));
 		
-		VBox principal= new VBox();
+		Button newTask = new Button("Nueva Tarea");
+		FontIcon icon= new FontIcon("fas-plus");
+		newTask.setGraphic(icon);
+		newTask.setContentDisplay(ContentDisplay.RIGHT);
+		
+		
+		VBox principal= new VBox(contTitulo,newTask);
 		Scene scene = new Scene(principal);
         scene.getStylesheets().add(getClass().getResource("/css/aplication.css").toExternalForm());
         stage.setScene(scene);
